e8caeba55175fdbf9514a9f2b90084ec6927c792
Srujan GV <srujangv05@gmail.com>
2022-09-14 17:59:46 +0530

    Added dependencies and fragments

--- app/src/main/java/com/pixelfly/trukisan/MainActivity.kt
+++ app/src/main/java/com/pixelfly/trukisan/MainActivity.kt
@@ -2,10 +2,13 @@ package com.pixelfly.trukisan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.ibrahimsn.lib.SmoothBottomBar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
