ac6a53f6f13eb5552bd606bc2bc6dd4f37cf845d
Srujan GV <srujangv05@gmail.com>
2022-09-14 18:40:20 +0530

    bottombar with fragments working

--- app/src/main/java/com/pixelfly/trukisan/MainActivity.kt
+++ app/src/main/java/com/pixelfly/trukisan/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.pixelfly.trukisan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import me.ibrahimsn.lib.SmoothBottomBar
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.pixelfly.trukisan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = findNavController(R.id.main_fragment)
+        //setupActionBarWithNavController(navController)
+        setupSmoothBottomMenu()
+    }
 
+    private fun setupSmoothBottomMenu() {
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.menu)
+        val menu = popupMenu.menu
+        binding.bottomBar.setupWithNavController(menu, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
