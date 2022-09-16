72a706147f4422678a99239d9aeab65cec78b584
Srujan GV <srujangv05@gmail.com>
2022-09-16 19:45:10 +0530

    Custom Action bar added for all fragments

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/pixelfly/trukisan/databinding/FragmentHomeBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/pixelfly/trukisan/databinding/FragmentHomeBinding.java
@@ -7,17 +7,24 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.pixelfly.trukisan.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class FragmentHomeBinding implements ViewBinding {
   @NonNull
   private final FrameLayout rootView;
 
-  private FragmentHomeBinding(@NonNull FrameLayout rootView) {
+  @NonNull
+  public final Toolbar toolbar;
+
+  private FragmentHomeBinding(@NonNull FrameLayout rootView, @NonNull Toolbar toolbar) {
     this.rootView = rootView;
+    this.toolbar = toolbar;
   }
 
   @Override
@@ -43,10 +50,19 @@ public final class FragmentHomeBinding implements ViewBinding {
 
   @NonNull
   public static FragmentHomeBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.toolbar;
+      Toolbar toolbar = ViewBindings.findChildViewById(rootView, id);
+      if (toolbar == null) {
+        break missingId;
+      }
 
-    return new FragmentHomeBinding((FrameLayout) rootView);
+      return new FragmentHomeBinding((FrameLayout) rootView, toolbar);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
