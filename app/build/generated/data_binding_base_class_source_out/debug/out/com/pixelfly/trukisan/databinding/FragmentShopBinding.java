09084a2a2e34588e453ab4d5475e74340a547a45
shashankt383 <shashankt383@gmail.com>
2022-09-28 13:00:13 +0530

    Shop fragment

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/pixelfly/trukisan/databinding/FragmentShopBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/pixelfly/trukisan/databinding/FragmentShopBinding.java
@@ -4,25 +4,55 @@ package com.pixelfly.trukisan.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.pixelfly.trukisan.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class FragmentShopBinding implements ViewBinding {
   @NonNull
-  private final FrameLayout rootView;
+  private final ConstraintLayout rootView;
 
-  private FragmentShopBinding(@NonNull FrameLayout rootView) {
+  @NonNull
+  public final TextView TextView;
+
+  @NonNull
+  public final ImageView imageView2;
+
+  @NonNull
+  public final ImageView imageView3;
+
+  @NonNull
+  public final ImageView imageView4;
+
+  @NonNull
+  public final ImageView imageView5;
+
+  @NonNull
+  public final ImageView imageView6;
+
+  private FragmentShopBinding(@NonNull ConstraintLayout rootView, @NonNull TextView TextView,
+      @NonNull ImageView imageView2, @NonNull ImageView imageView3, @NonNull ImageView imageView4,
+      @NonNull ImageView imageView5, @NonNull ImageView imageView6) {
     this.rootView = rootView;
+    this.TextView = TextView;
+    this.imageView2 = imageView2;
+    this.imageView3 = imageView3;
+    this.imageView4 = imageView4;
+    this.imageView5 = imageView5;
+    this.imageView6 = imageView6;
   }
 
   @Override
   @NonNull
-  public FrameLayout getRoot() {
+  public ConstraintLayout getRoot() {
     return rootView;
   }
 
@@ -43,10 +73,50 @@ public final class FragmentShopBinding implements ViewBinding {
 
   @NonNull
   public static FragmentShopBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.TextView;
+      TextView TextView = ViewBindings.findChildViewById(rootView, id);
+      if (TextView == null) {
+        break missingId;
+      }
 
-    return new FragmentShopBinding((FrameLayout) rootView);
+      id = R.id.imageView2;
+      ImageView imageView2 = ViewBindings.findChildViewById(rootView, id);
+      if (imageView2 == null) {
+        break missingId;
+      }
+
+      id = R.id.imageView3;
+      ImageView imageView3 = ViewBindings.findChildViewById(rootView, id);
+      if (imageView3 == null) {
+        break missingId;
+      }
+
+      id = R.id.imageView4;
+      ImageView imageView4 = ViewBindings.findChildViewById(rootView, id);
+      if (imageView4 == null) {
+        break missingId;
+      }
+
+      id = R.id.imageView5;
+      ImageView imageView5 = ViewBindings.findChildViewById(rootView, id);
+      if (imageView5 == null) {
+        break missingId;
+      }
+
+      id = R.id.imageView6;
+      ImageView imageView6 = ViewBindings.findChildViewById(rootView, id);
+      if (imageView6 == null) {
+        break missingId;
+      }
+
+      return new FragmentShopBinding((ConstraintLayout) rootView, TextView, imageView2, imageView3,
+          imageView4, imageView5, imageView6);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
